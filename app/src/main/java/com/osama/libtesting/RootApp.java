cec297b5e304040e7f189caf1ea63e19c0f31b4f
Osama <oussama2ste@gmail.com>
2020-11-02 21:17:32 +0100

    Downgrade the gradle version

--- app/src/main/java/com/osama/libtesting/RootApp.java
+++ app/src/main/java/com/osama/libtesting/RootApp.java
@@ -16,8 +16,8 @@ public class RootApp extends Application {
 
     public static synchronized void setupMobiOptionsAds(MobiInitializationListener listener) {
         if (mobiOptionsAdsInit == null) {
-            MobiOptionsAdsInit.setAdmobTestDevices(Collections.singletonList("YOU-TEST-DEVICE-ID-PLACED-HERE"));
-            MobiOptionsAdsInit.setDisableStoreCheck(true);                                  // true to simulate the play sotre behaviour
+            MobiOptionsAdsInit.setAdmobTestDevices(Collections.singletonList("YOUR-TEST-DEVICE-ID-PLACED-HERE"));
+            MobiOptionsAdsInit.setDisableStoreCheck(true);                                  // true to simulate the play store behaviour
             mobiOptionsAdsInit = MobiOptionsAdsInit.build(rootApplication,
                     "TJ6N6Wy8aZsc9oWW92TuXlIZwsGtj7",
                     true,                                                       // If you set this to false, the list of the test devices will be ignored.
